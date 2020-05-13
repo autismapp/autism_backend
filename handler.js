d7493c58179ef8173a6b780b314747dd6bf1124e
shona10 <shonamason10@gmail.com>
2020-05-13 20:53:35 +0100

    All activity endpoints

--- handler.js
+++ handler.js
@@ -16,8 +16,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/activity-type', (request, response) => {
-  connection.query('SELECT * FROM activity_type', (err,data) => {
-    if (err){
+  connection.query('SELECT * FROM activity_type', (err, data) => {
+    if (err) {
       console.log('Error from MySql', err);
       response.status(500).send(err);
     } else {
@@ -25,5 +25,44 @@ app.get('/activity-type', (request, response) => {
     }
   });
 });
+app.get('/activity', (request, response) => {
+  connection.query('SELECT a.*, at.image_url FROM activity a INNER JOIN activity_type at ON a.activity_type_id = at.activity_type_id', (err, data) => {
+    if (err) {
+      console.log('Error from MySql', err);
+      response.status(500).send(err);
+    } else {
+      response.status(200).send(data);
+    }
+  });
+});
+app.put('/activity/:id', (request, response) => {
+  const id = request.params.id;
+  const data = request.body;
+  // Write an SQL query to update the fields provided in the request for the task WHERE TaskId = id
+  // Remember to escape user-provided values
+  // Send back 200 (not the updated task)
+  connection.query(`UPDATE activity SET Completed = ${data.completed} WHERE activity_id = ${id}`, (err) => {
+    if (err) {
+      console.log('Error from MySql', err);
+      response.status(500).send(err);
+    } else {
+      response
+        .status(200)
+        .send(`Updated actvity with ID ${id} and data ${JSON.stringify(data)}`);
+    }
+  });
+});
 
+app.post('/activity/', (request, response) => {
+  const data = request.body;
+  connection.query(`INSERT INTO activity (user_id, activity_type_id, completed) VALUES (1, ${data.activity_type_id}, 0)`, (err) => {
+    if (err) {
+      response.status(500).send(err);
+    } else {
+      response
+        .status(200)
+        .send(`Created new activity`);
+    }
+  });
+});
 module.exports.app = serverlessHttp(app);
